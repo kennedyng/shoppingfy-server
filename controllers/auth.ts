a3d77c5fc8aeff3368f9a74a06f44dcc71ae016d
Kennedy Ngosa <kennedyngosachanda@gmail.com>
2024-03-11 21:56:42 +0200

    register end point

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,15 +1,42 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import prisma from "../lib/utils/prisma";
 
-const loginUser = async (req: Request, res: Response): Promise<void> => {
+const loginUser = async (req: Request, res: Response) => {
   const data = { msg: "hello!" };
   console.log(data);
   res.json(data);
 };
 
-const registerUser = async (req: Request, res: Response): Promise<void> => {
-  const data = { msg: "hello!" };
-  console.log(data);
-  res.json(data);
+const registerUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+
+    const accountExists = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!accountExists) {
+      const createdData = await prisma.user.create({
+        data: {
+          email,
+          password,
+        },
+      });
+
+      return res.status(201).json(createdData);
+    }
+    return res
+      .status(409)
+      .json({ message: "Auth Failed Account Already Exists" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export { loginUser, registerUser };
--- middlewares/errors.ts
+++ middlewares/errors.ts
@@ -6,6 +6,5 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.error(err);
   res.status(500).send({ errors: [{ message: "Something went wrong" }] });
 };
