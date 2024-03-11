c882ea8eded201b6cfee4551c18a641e51b48710
Kennedy Ngosa <kennedyngosachanda@gmail.com>
2024-03-11 21:15:26 +0200

    new changes

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import dotenv from "dotenv";
 import express, { Application } from "express";
 import { authRouter } from "./routes/authRoute";
+import { errorHandler } from "./middlewares/errors";
+import { json } from "body-parser";
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8080;
-
+app.use(json());
+app.use(errorHandler);
 app.use("/api/auth", authRouter);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
