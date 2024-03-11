805382038a0ca7f44c7f4dfa355d3d46317eb71f
Kennedy Ngosa <kennedyngosachanda@gmail.com>
2024-03-11 22:39:46 +0200

    new changes

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,10 +1,39 @@
 import { NextFunction, Request, Response } from "express";
 import prisma from "../lib/utils/prisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const loginUser = async (req: Request, res: Response) => {
-  const data = { msg: "hello!" };
-  console.log(data);
-  res.json(data);
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user by email
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    // Verify password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      console.log("matching pawwa!", user.password);
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    // Create JWT token
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRETE as string,
+      {
+        expiresIn: "1h", // Token expires in 1 hour
+      }
+    );
+
+    // Return token in response
+    res.status(200).json({ token, id: user.id, email: user.email });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const registerUser = async (
@@ -22,10 +51,19 @@ const registerUser = async (
     });
 
     if (!accountExists) {
+      // Hash password
+      const hashedPassword = await bcrypt.hash(password, 10); // 10 is the salt rounds
+
       const createdData = await prisma.user.create({
         data: {
           email,
-          password,
+          password: hashedPassword,
+        },
+        select: {
+          email: true,
+          createdAt: true,
+          id: true,
+          updatedAt: true,
         },
       });
 
--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,6 +1,7 @@
 // routes.ts
 import { Router, Request, Response } from "express";
 import { loginUser, registerUser } from "../controllers/auth";
+import { body } from "express-validator";
 
 const authRouter = Router();
 
